c8cc35db473b485cf9c09770c3df14427008cbe2
anhha19122005 <anhha19122005@gmail.com>
2026-09-17 09:36:13 +0700

    fix: resolve login prerender error

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import LoginForm from "../ui/login-form";
 
 export default function LoginPage() {
     return (
         <>
-            <LoginForm/>
+            <Suspense fallback={<div>Loading...</div>}>
+                <LoginForm/>
+            </Suspense>
         </>
     )
 }
